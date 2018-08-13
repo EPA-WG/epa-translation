87f3486d833981cc138b078abb5cc0f7d57c2548
sashafir <suns@firsov.net>
2018-08-12 19:06:10 -0700

    embedded nodes not editable.

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 * en.i18n.json compatibility
+* evaluate https://en.wikipedia.org/wiki/InterPlanetary_File_System as storage
--- epa-translation.js
+++ epa-translation.js
@@ -11,21 +11,24 @@
                         form>*{ display:block }
                         
                    `.split('\n').map( s => s.trim() && `${scopeSelector} ${s}` ).join('\n')
-        ,   html = `<style> *[contentEditable]{background-color:aqua}
-                            *[contentEditable]:focus{background-color:aquamarine}
+        ,   html = `<style> *[contentEditable=true],
+                            *[contentEditable=true] *[contentEditable=false]{background-color:aqua}
+                            *[contentEditable=true]:focus{background-color:aquamarine}
+                            /**[contentEditable=false] *:focus{background-color:aquamarine}*/
                             #epa-translation-visibility{ display:none; }
                             ${css}
                     </style>
                     <input type="checkbox" id="epa-translation-visibility" />
                     <label for="epa-translation-visibility">&#x1F3F3; Languages</label>
                     <form>
-                        <label> Vocabulary URL <input type="text" /></label>
                         <label 
                             class="epa-translation-edit"
                             accesskey="t" 
                     title="Trigger Translation Editor mode
                     ALT+T or CTRL+ALT+T"
                         ><input type="checkbox"/> <u><b>T</b></u>ranslation editor mode </label>
+                        <label class="epa-trans-props-lang"> Language <input type="text" value="unknown"/></label>
+                        <label class="epa-trans-props-dict"> Dictionary URL <input type="text" placeholder="" /></label>
                     </form>
                     `;
         $conf.innerHTML = html;
@@ -37,18 +40,25 @@
         {
             document.body[ bEnable? 'addEventListener': 'removeEventListener']( 'click',clickInEditMode,true );
             document.body[ bEnable? 'addEventListener': 'removeEventListener']( 'input',inputInEditMode,true );
+            document.body[ bEnable? 'addEventListener': 'removeEventListener']( 'blur' , blurInEditMode,true );
             bEnable || document.body.querySelectorAll('*[contentEditable]').forEach( el=> el.removeAttribute('contentEditable'));
         }
             function
         clickInEditMode(ev)
-        {   const el = ev.target;
-            // if( el.childElementCount )
-            //     return;
+        {
+            let el = ev.target;
+            el !== editedEl && clearContentEditable( editedEl );
             editedEl = el;
             let t = el.origText;
             if( !t )
                 el.origText = el.innerText;
             el.contentEditable = "true";
+            if( el.children )
+                for( let c of el.children )
+                    c.contentEditable = "false";
+            while( el = el.parentNode )
+                if( el.isContentEditable )
+                    el.contentEditable = "false";
         }
             function
         inputInEditMode(ev)
@@ -58,6 +68,21 @@
             if( txt === el.origText || txt === el.savedText )
                 return;
             localStorage.setItem( getStorageKey(el.origText), el.savedText=txt );
+        }
+            function
+        blurInEditMode(ev)
+        {
+            clearContentEditable(ev.target)
+        }
+            function
+        clearContentEditable( el )
+        {   if( !el )
+                return;
+            if( el.origText && el.innerText === el.origText )
+                el.contentEditable = "inherit";
+            if( el.children )
+                for( let c of el.children )
+                    c.contentEditable = "false";
         }
             function
         getStorageKey( txt )
